75aac3e3740172858fd74cd42c409460354dc52c
Joe Dolson <design@joedolson.com>
2023-03-19 11:24:58 -0500

    Remove custom header feature
    Unused

--- src/wp-a11y-day/functions.php
+++ src/wp-a11y-day/functions.php
@@ -248,11 +248,6 @@ function wp_accessibility_day_scripts() {
 add_action( 'wp_enqueue_scripts', 'wp_accessibility_day_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
