fdb909b78741c8e3c698b961a6e31a5e4b62e6f6
Joe Dolson <design@joedolson.com>
2023-03-19 11:06:08 -0500

    Remove conference talk timezone handling from theme

--- src/wp-a11y-day/js/talk-time.js
+++ src/wp-a11y-day/js/talk-time.js
@@ -1,25 +1,9 @@
 (function ($) {
 	'use strict';
 	$(function () {
-		var zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-		// Handle Internet Explorer's lack of timezone info.
-		if ( undefined === zone ) {
-			zone = 'your local time';
-		}
-		$( '.event-time' ).each( function ( index ) {
-			var utcTime  = $( this ).attr( 'data-time' );
-			var userTime = new Date( utcTime ).toLocaleTimeString().replace( ':00 ', ' ' );
-			var userDate = new Date( utcTime ).toLocaleDateString();
-
-			$( this ).append( '<span class="localtime">' + userDate + ' at ' + userTime + ' ' + zone + '</span>' );
-		});
-		$( 'h2.talk-time' ).each( function ( index ) {
-			var utcTime  = $( this ).attr( 'data-time' );
-			var userTime = new Date( utcTime ).toLocaleTimeString().replace( ':00 ', ' ' );
-
-			$( this ).find( '.time-wrapper' ).append( '<span class="localtime">' + userTime + ' ' + zone + '</span>' );
-		});
-
+		/**
+		 * Render speaker application input times to local timezones.
+		 */
 		$( '#input_8_36 .gfield-choice-input' ).each( function( index ) {
 			var id    = $( this ).attr( 'id' );
 			var label = $( 'label[for=' + id + ']' );
