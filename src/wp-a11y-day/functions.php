a569692f6e6ceff99ffa884cd37390da16019cf5
Joe Dolson <design@joedolson.com>
2023-03-19 11:27:31 -0500

    Remove custom background

--- src/wp-a11y-day/functions.php
+++ src/wp-a11y-day/functions.php
@@ -57,12 +57,6 @@ if ( ! function_exists( 'wp_accessibility_day_setup' ) ) :
 			'caption',
 		) );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
-
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
@@ -135,7 +129,6 @@ if ( ! function_exists( 'wp_accessibility_day_setup' ) ) :
 	}
 endif;
 add_action( 'after_setup_theme', 'wp_accessibility_day_setup' );
-
 
 /**
  * Register Widgets.
