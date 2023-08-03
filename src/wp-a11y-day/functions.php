f7605ffda3d354d2299e606fb2890ece33f4cea1
Joe Dolson <design@joedolson.com>
2023-08-03 13:16:46 -0500

    Add large square image size
    Fixes #30

--- src/wp-a11y-day/functions.php
+++ src/wp-a11y-day/functions.php
@@ -38,6 +38,7 @@ if ( ! function_exists( 'wp_accessibility_day_setup' ) ) :
 		 */
 		add_theme_support( 'post-thumbnails' );
 		add_image_size( 'archive-image', 704, 392, array( 'left', 'top' ) );
+		add_image_size( 'large-square', 720, 720, true );
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
